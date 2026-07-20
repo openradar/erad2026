c7892c950a5bd3b57aba57b450084a0fc69c58c4
Kai Mühlbauer <kai.muehlbauer@uni-bonn.de>
2026-07-20 10:53:00 +0200

    jupyter server config and jupyterlab settings

---
+++ binder/jupyter_server_config.py
@@ -0,0 +1,4 @@
+# Show disk usage in the JupyterLab resource monitor
+c.ResourceUseDisplay.track_disk_usage = True
+# Measure usage of the user's workspace
+c.ResourceUseDisplay.disk_path = "/home/jovyan"
